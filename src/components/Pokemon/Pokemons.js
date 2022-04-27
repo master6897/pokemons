c8d9e97b9a9990b59b95add112001bcb344935e2
Marcin Puc <marcinpuc@MacBook-Pro-Marcin.local>
2022-04-27 12:43:42 +0200

    Changes

--- src/components/Pokemon/Pokemons.js
+++ src/components/Pokemon/Pokemons.js
@@ -8,61 +8,67 @@ import Button from "../Helpers/Button/Buttons";
 import Filter from "../Filter/Filter";
 import Loading from "../Helpers/Loading/Loading";
 
-
 const StyledContainer = styled.article`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    padding: 2rem 0;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 2rem 0;
+`;
 const Pokemons = () => {
-    const dispatch = useDispatch();
-    const [isDone, setIsDone] = useState(false);
-    const [moreDataRequest, setMoreDataRequest] = useState(false);
-    const [offset, setOffset] = useState(20);
-    const isFiltered = useSelector(state => state.pokemons.isFiltered);
-    let promises;
-    
-    const applyData = async (allPokemons) => {
-        allPokemons.results.forEach(async (pokemon) => {
-            promises = new Promise(async (resolve) => {
-                let url = pokemon.url;
-                await fetch(url)
-                .then(response => response.json())
-                .then(pokemon => {
-                    resolve(dispatch(pokemonsAction.addPokemonsToStore(pokemon)));
-                });
-            })  
-        });
-        Promise.all([promises]).then(() => {
-            setIsDone(true);
-            setMoreDataRequest(false);
-            dispatch(pokemonsAction.addPokemonsToDisplay());
-        })
-    };
+  const dispatch = useDispatch();
+  const [isDone, setIsDone] = useState(false);
+  const [moreDataRequest, setMoreDataRequest] = useState(false);
+  const [offset, setOffset] = useState(20);
+  const isFiltered = useSelector((state) => state.pokemons.isFiltered);
+  let promises;
 
-    const {isLoading, error, sendRequest: fetchPokemons} = useHttp(applyData);
-    useEffect(() => {
-        fetchPokemons({url:`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`});
-        setMoreDataRequest(true);
-    },[offset]);
-    const loadMoreDataHandler = () => {
-        setOffset((prevState) => {return prevState+20});
-    }
-    return(
-        <StyledContainer>
-            {!error && <Filter />}
-            {!isLoading && isDone && !error && <PokemonsList />}
-            {!isFiltered && isDone && !moreDataRequest && !error &&<Button value={'Load more...'} onClick={loadMoreDataHandler}/>}
-            {(isLoading || !isDone || moreDataRequest) && !error && (
-                <>
-                    <Loading />
-                    <h1>Loading...</h1>
-                </>)}
-            {!isLoading && error && <h1>Something went wrong!</h1>}
-        </StyledContainer>
-    )
-}
+  const applyData = async (allPokemons) => {
+    allPokemons.results.forEach(async (pokemon) => {
+      promises = new Promise(async (resolve) => {
+        let url = pokemon.url;
+        await fetch(url)
+          .then((response) => response.json())
+          .then((pokemon) => {
+            resolve(dispatch(pokemonsAction.addPokemonsToStore(pokemon)));
+          });
+      });
+    });
+    Promise.all([promises]).then(() => {
+      setIsDone(true);
+      setMoreDataRequest(false);
+      dispatch(pokemonsAction.addPokemonsToDisplay());
+    });
+  };
+
+  const { isLoading, error, sendRequest: fetchPokemons } = useHttp(applyData);
+  useEffect(() => {
+    fetchPokemons({
+      url: `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`,
+    });
+    setMoreDataRequest(true);
+  }, [offset, fetchPokemons]);
+  const loadMoreDataHandler = () => {
+    setOffset((prevState) => {
+      return prevState + 20;
+    });
+  };
+  return (
+    <StyledContainer>
+      {!error && <Filter />}
+      {!isLoading && isDone && !error && <PokemonsList />}
+      {!isFiltered && isDone && !moreDataRequest && !error && (
+        <Button value={"Load more..."} onClick={loadMoreDataHandler} />
+      )}
+      {(isLoading || !isDone || moreDataRequest) && !error && (
+        <>
+          <Loading />
+          <h1>Loading...</h1>
+        </>
+      )}
+      {!isLoading && error && <h1>Something went wrong!</h1>}
+    </StyledContainer>
+  );
+};
 export default Pokemons;
